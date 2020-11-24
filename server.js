5f6c89b6389fa4e31eff43e2362d24b2991852eb
AnjinIsmail <anjin.ismail@gmail.com>
2020-11-24 14:45:19 -0800

    deployed

--- server.js
+++ server.js
@@ -50,6 +50,21 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -58,14 +73,10 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-
-
-
-
-
-
-
-
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
+});
 
 
 
